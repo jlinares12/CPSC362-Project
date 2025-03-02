acc992568b565fc94df89c0b72193fd87441195b
jlinares12 <142842452+jlinares12@users.noreply.github.com>
2025-03-02 18:47:49 +0000

    I created a new python file that will have our form classes (register form  and  login form). Currently it only holds our registration form

---
+++ community_garden/forms.py
@@ -0,0 +1,11 @@
+# run in terminal on laptop: pip install flask-wtf
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+
+class RegisterForm( FlaskForm ):
+    name = StringField( label='name' )
+    username = StringField( label='username' )
+    email_address = StringField( label='email' )
+    password1 = PasswordField( label='password1' )
+    password2 = PasswordField( label='password2' )
+    submit = SubmitField( label='Sign up' )
