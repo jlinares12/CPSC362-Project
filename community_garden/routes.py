cdca24017a5e64bf16796dabce7852a90f355c40
Juan Linares <juanlinares821@csu.fullerton.edu>
2025-03-02 14:32:21 -0800

    added code that would add a user to our database with valid data. If any of the forms inputs is invalid I added a flash warning that would get sent to the client to see, If a valid user is created the client would get redirected to the login page

--- community_garden/routes.py
+++ community_garden/routes.py
@@ -1,7 +1,8 @@
 from community_garden import app
-from flask import render_template
+from flask import render_template, redirect, url_for, flash
 from community_garden.models import User
 from community_garden.forms import RegisterForm
+from community_garden import db
 
 @app.route("/")
 @app.route("/home")
@@ -28,7 +29,18 @@ def about_page():
 def login_page():
     return render_template('login_page.html')
 
-@app.route('/register')
+@app.route('/register', methods=['GET','POST'])
 def register_page():
     form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(name=form.name.data,
+                              username=form.username.data,
+                              email=form.email.data,
+                              password_hash=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('login_page'))
+    if form.errors != {}:                                           # If there are errors from the validations
+        for err_msg in form.errors.values():
+            flash(f'There was an error with creating a user: {err_msg}', category='danger')
     return render_template('register_page.html', form=form)
