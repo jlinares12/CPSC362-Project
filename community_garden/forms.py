cb8fbaeeaa96d73910c635abb1f79ad007b0085d
Juan Linares <juanlinares821@csu.fullerton.edu>
2025-03-02 14:28:50 -0800

    I added validators to each of the variables in the RegisterForm class

--- community_garden/forms.py
+++ community_garden/forms.py
@@ -1,11 +1,12 @@
 # run in terminal on laptop: pip install flask-wtf
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Length, EqualTo, Email, DataRequired
 
 class RegisterForm( FlaskForm ):
-    name = StringField( label='name' )
-    username = StringField( label='username' )
-    email_address = StringField( label='email' )
-    password1 = PasswordField( label='password1' )
-    password2 = PasswordField( label='password2' )
+    name = StringField( label='name', validators=[Length(min=3, max=50), DataRequired()] )
+    username = StringField( label='username', validators=[Length(max=65), Email(), DataRequired()])
+    email = StringField( label='email', validators=[Length(min=3, max=65), DataRequired()])
+    password1 = PasswordField( label='password1', validators=[Length(min=10), DataRequired()] )
+    password2 = PasswordField( label='password2', validators=[EqualTo('password1'), DataRequired()] )
     submit = SubmitField( label='Sign up' )
