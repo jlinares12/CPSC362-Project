8308b8d4e7c087829bd43bbbe5146342635bb07e
jlinares12 <142842452+jlinares12@users.noreply.github.com>
2025-03-02 18:49:49 +0000

    had to change our __init__.py file so that we could configure a secret key to our flask app so that we could prevent CSRF

--- community_garden/__init__.py
+++ community_garden/__init__.py
@@ -3,7 +3,10 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)   # Create an instance of a FLASK app called app
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db' # Tells python where the sqlite database is stored
+
+app.config['SECRET_KEY'] = 'd4d850ac4be6b386b7b3c5ab' # Saves a secret key to be able to prevent CSRF
 
 db = SQLAlchemy(app)    # Create an instance of SQLAlchemy called db that takes our flask app as a parameter
 
