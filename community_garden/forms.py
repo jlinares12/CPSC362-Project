8eecc4054b0fad31c21213b0cb0f4982cccd66d3
jlinares12 <142842452+jlinares12@users.noreply.github.com>
2025-03-03 20:42:30 +0000

    added two validation functions to ensure that a user does not try to create an account with a username or password that already exists

--- community_garden/forms.py
+++ community_garden/forms.py
@@ -1,9 +1,20 @@
 # run in terminal on laptop: pip install flask-wtf
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import Length, EqualTo, Email, DataRequired
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
+from community_garden.models import User
 
 class RegisterForm( FlaskForm ):
+    def validate_username(self, username_to_validate):
+        user = User.query.filter_by(username=username_to_validate.data).first()
+        if user:
+            raise ValidationError('Username already exists!')
+
+    def validate_email(self, email_to_validate):
+        email = User.query.filter_by( email=email_to_validate.data ).first()
+        if email:
+            raise ValidationError('Email address already exists!')
+
     name = StringField( label='name', validators=[Length(min=3, max=50), DataRequired()] )
     username = StringField( label='username', validators=[Length(max=65), DataRequired()])
     email = StringField( label='email', validators=[Length(min=3, max=65), Email(), DataRequired()])
