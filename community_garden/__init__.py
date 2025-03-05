b3f408dc936299a0f9fd134a79efdeea08e979e1
jlinares12 <142842452+jlinares12@users.noreply.github.com>
2025-03-05 19:07:36 +0000

    made it so that our sql URI and our secret_key is stored in a .env file locally on my computer. updated the gitignore file to ignore the .env file

--- community_garden/__init__.py
+++ community_garden/__init__.py
@@ -4,10 +4,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 #pip install flask_login
 from flask_login import LoginManager
+from dotenv import dotenv_values
 
+secrets = dotenv_values(".env")
 app = Flask(__name__)   # Create an instance of a FLASK app called app
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db' # Tells python where the sqlite database is stored
-app.config['SECRET_KEY'] = 'd4d850ac4be6b386b7b3c5ab' # Saves a secret key to be able to prevent CSRF
+app.config['SQLALCHEMY_DATABASE_URI'] = secrets['DATABASE_URI'] # Tells python where the sqlite database is stored
+app.config['SECRET_KEY'] = secrets['DEV_KEY'] # Saves a secret key to be able to prevent CSRF
 db = SQLAlchemy(app)    # Create an instance of SQLAlchemy called db that takes our flask app as a parameter
 bcrypt = Bcrypt(app)    # Create an instance of Bcrypt using our app as a parameter
 login_manager = LoginManager(app)
