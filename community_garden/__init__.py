ec993b2436d509f654403c1ca4a9d9501c5fb22c
Juan Linares <juanlinares821@csu.fullerton.edu>
2025-03-06 23:42:52 -0800

    imported flask_uploads so people can upload a picture of their garden, added configurations so that the picture files get validated and added to a directory

--- community_garden/__init__.py
+++ community_garden/__init__.py
@@ -3,11 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from dotenv import dotenv_values
+from flask_uploads import  configure_uploads, IMAGES, UploadSet
 
 secrets = dotenv_values(".env")
 app = Flask(__name__)   # Create an instance of a FLASK app called app
 app.config['SQLALCHEMY_DATABASE_URI'] = secrets['DATABASE_URI'] # Tells python where the sqlite database is stored
 app.config['SECRET_KEY'] = secrets['DEV_KEY'] # Saves a secret key to be able to prevent CSRF
+app.config['UPLOADED_PHOTOS_DEST'] = 'community_garden/static/img'
+photos = UploadSet('photos', IMAGES)
+configure_uploads(app, photos)
 db = SQLAlchemy(app)    # Create an instance of SQLAlchemy called db that takes our flask app as a parameter
 bcrypt = Bcrypt(app)    # Create an instance of Bcrypt using our app as a parameter
 login_manager = LoginManager(app)
