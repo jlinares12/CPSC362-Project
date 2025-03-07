d19ecc931787676d2fc49cf4b0de96bed1e41afa
jlinares12 <142842452+jlinares12@users.noreply.github.com>
2025-03-07 22:29:16 +0000

    fixed visual bug that squished the sidebar links while the sidebar was opening

--- community_garden/static/js/sidebar.js
+++ community_garden/static/js/sidebar.js
@@ -1,8 +1,4 @@
 function toggleSidebar() {
     const sidebar = document.getElementById('sidebar');
-    if (sidebar.style.width === '250px') {
-        sidebar.style.width = '0';
-    } else {
-        sidebar.style.width = '250px';
-    }
+    sidebar.classList.toggle('open');
 }
