08e7e8c88b0abfb621cbf7c26f913021e1205299
Juan Linares <juanlinares821@csu.fullerton.edu>
2025-03-14 16:06:38 -0700

    Imported two libraries. geopy.geocoders is to get the coordinates of our gardens, and flask_migrate is to migrate changes made to our models without having to drop our tables in the backend. I then initialized an object called migrate that creates an Instance of Migrate with our app and db as parameters, I also created an object called geolocator that instantiated Nominatim

--- community_garden/__init__.py
+++ community_garden/__init__.py
@@ -4,6 +4,8 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from dotenv import dotenv_values
 from flask_uploads import  configure_uploads, IMAGES, UploadSet
+from geopy.geocoders import Nominatim
+from flask_migrate import Migrate
 
 secrets = dotenv_values(".env")
 app = Flask(__name__)   # Create an instance of a FLASK app called app
@@ -14,9 +16,11 @@ photos = UploadSet('photos', IMAGES)
 jawg_token = secrets['JAWG_API_KEY']
 configure_uploads(app, photos)
 db = SQLAlchemy(app)    # Create an instance of SQLAlchemy called db that takes our flask app as a parameter
+migrate = Migrate(app, db)
 bcrypt = Bcrypt(app)    # Create an instance of Bcrypt using our app as a parameter
 login_manager = LoginManager(app)
 login_manager.login_view = 'login_page'
 login_manager.login_message_category = 'danger'
 
+geolocator = Nominatim(user_agent="community_garden_flask_app")
 from community_garden import routes
