2c3d75a166ed4dc19b5ee789f09753cacfe468f7
Juan Linares <juanlinares821@csu.fullerton.edu>
2025-04-22 15:00:49 -0700

    fixed bug that made it so that the hamburger only worked after refreshing page

--- community_garden/static/js/responsiveBasePage.js
+++ community_garden/static/js/responsiveBasePage.js
@@ -1,10 +1,7 @@
-if (window.visualViewport.width <= 704) {
-  hamburger = document.querySelector(".hamburger");
+hamburger = document.querySelector(".hamburger");
 
-  hamburger.onclick = function() {
-    navBar = document.querySelector(".nav-box");
-    navBar.classList.toggle("active")
-    console.log("click works");
-  }
+hamburger.onclick = function() {
+  navBar = document.querySelector(".nav-box");
+  navBar.classList.toggle("active")
+  console.log("click works");
 }
-
